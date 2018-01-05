09b4b7056772ad2d8759d9ef06470591f30b2ac4
Jayaram <vjayaram01@gmail.com>
2018-01-05 13:21:08 +0530

    alignmet done

--- app/Models/Salaries.js
+++ app/Models/Salaries.js
@@ -4,33 +4,33 @@ const Model = use('Model');
 
 class Salaries extends Model {
 
-  Empsal () {
-    return this.belongsTo('App/Models/Employee','emp_no','emp_no')
+  Empsal() {
+    return this.belongsTo('App/Models/Employee', 'emp_no', 'emp_no')
   }
-  static get createdAtColumn () {
+  static get createdAtColumn() {
     return null
   }
-  static get updatedAtColumn () {
+  static get updatedAtColumn() {
     return null
   }
-  static get Addempsal(){
+  static get Addempsal() {
     return {
       salary: 'required',
       from_date: 'date_format:YYYY-MM-DD|required',
       to_date: 'date_format:YYYY-MM-DD|required'
     }
   }
-    static get Addempsalary(){
-        return {
-            emp_no:'required',
-            salary: 'required',
-            from_date: 'date_format:YYYY-MM-DD|required',
-            to_date: 'date_format:YYYY-MM-DD|required'
-        }
+  static get Addempsalary() {
+    return {
+      emp_no: 'required',
+      salary: 'required',
+      from_date: 'date_format:YYYY-MM-DD|required',
+      to_date: 'date_format:YYYY-MM-DD|required'
     }
-  static get AddEmpSalError(){
-    return{
-      'emp_no.required':'emp_no.required',
+  }
+  static get AddEmpSalError() {
+    return {
+      'emp_no.required': 'emp_no.required',
       'from_date.date_format': 'date format should be like this:YYYY-MM-DD',
       'salary.required': 'salary field required',
       'from_date.required': 'from_date should be need',
